b4a374fc98a5e97436868a94c343fb05e4a05333
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-06-03 01:42:29 +0900

    #15.1 React Hook Form

--- frontend/src/routers/logged-out-router.tsx
+++ frontend/src/routers/logged-out-router.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { isLoggedInVar } from 'src/apollo'
 
 function LoggedOutRouter() {
+    const onClick = () => {
+        isLoggedInVar(true)
+    }
     return (
         <div>
             
